aa13d7b61c9305b34e8a16f78cb43e4ce0cde80e
Prince chaudhary <princejatt243@gmail.com>
2026-06-25 21:42:43 +0530

    Time: 0 ms (100%), Space: 9.3 MB (50.66%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sSum=0,tSum=0;
-        for(char c:s){
-            sSum+=c;
-        }
-        for(char c:t){
-            tSum+=c;
-        }
-        char ans= tSum-sSum;
-        return ans;
-        
-    }
+    char ans = 0;
+    for (char c : s) ans ^= c;
+    for (char c : t) ans ^= c;
+    return ans;
+}
 };
