e543f6d20b9a881f4df68b928829ccccbe577708
Prince chaudhary <princejatt243@gmail.com>
2026-07-23 13:57:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int temp =x;
+        long long int  digit =0;
+        while(temp>0){
+            digit = digit*10 + (temp%10);
+            temp /=10;
+        }
+        if( digit == x) return true;
+        return false;
+        
+    }
+};
