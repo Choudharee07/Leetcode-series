954745d030c9b687e44a62315d5da117d10b8f0a
Prince chaudhary <princejatt243@gmail.com>
2026-07-23 21:22:31 +0530

    Time: 43 ms (49.68%), Space: 85.9 MB (75.98%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1) return 0;
+        int diff,maxi=0;
+        for(int i=1;i<nums.size();i++){
+            diff= nums[i]-nums[i-1];
+            maxi = max(maxi,diff);
+        }
+        return maxi;
+    }
+};
