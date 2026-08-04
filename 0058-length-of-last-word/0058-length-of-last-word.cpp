cee72264cb598a1d11a1aad1276129bd0c410f30
Prince chaudhary <princejatt243@gmail.com>
2026-08-04 12:19:39 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.85%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(isalnum(s[i])) count++;
+            else if(count>0) return count;
+            else continue;
+        }
+        return count;
+    }
+};
