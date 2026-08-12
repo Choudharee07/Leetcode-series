b8d1828a39552a831580c811ce7a6655e18f4357
Prince chaudhary <princejatt243@gmail.com>
2026-08-12 23:58:46 +0530

    Time: 0 ms (100%), Space: 17.3 MB (13.26%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        for( string s:details){
+            string age;
+            age+=s[11];
+            age+=s[12];
+            if(stoi(age)>60) count++;
+        }
+        return count;
+    }
+};
