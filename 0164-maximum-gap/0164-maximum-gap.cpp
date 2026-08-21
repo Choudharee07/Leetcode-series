1ce3d0c53992c680a71182b0065c51aa26072043
Prince chaudhary <princejatt243@gmail.com>
2026-08-21 23:35:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int mx = *max_element(nums.begin(),nums.end());
+
+
         if(nums.size()==1) return 0;
         int diff,maxi=0;
         for(int i=1;i<nums.size();i++){
