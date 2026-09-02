398f682df4d1d4bbc7fb0f78b5031531b24eefbc
Prince chaudhary <princejatt243@gmail.com>
2026-09-02 08:13:06 +0530

    Time: 2 ms (5.47%), Space: 7.9 MB (97.17%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int splitNum(int num) {
         int num1=0,num2=0;
-        int freq[10]={0};
+        vector<int>digit;
         while(num>0){
-            freq[num%10]++;
+            digit.push_back(num%10);
             num/=10;
         }
-        int ind =0;
-        for(int i=0;i<10;i++){
-            while(freq[i]--){
-                if(ind %2==0){
-                    num1 = num1*10+ i;
-                }else{
-                    num2 = num2*10+ i;
-                }
-                ind++;
+        sort(digit.begin(),digit.end());
+        for(int i=0;i<digit.size();i++){
+            if(i%2==0){
+                num1 = num1*10+ digit[i];
+            }else{
+                num2 = num2*10+digit[i];
             }
         }
         return num1+num2;
