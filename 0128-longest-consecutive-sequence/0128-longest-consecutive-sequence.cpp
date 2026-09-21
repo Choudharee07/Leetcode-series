d35876b78a0670fc494009e9e764404c7a3e98cc
Prince chaudhary <princejatt243@gmail.com>
2026-09-21 23:22:22 +0530

    Time: 89 ms (23.11%), Space: 89 MB (24.72%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,12 +5,11 @@ public:
         unordered_set<int>seen(nums.begin(),nums.end());
         for(int val:seen){
             if(seen.count(val-1)) continue;
-            int count=1;
+            int i=1;
             if(seen.count(val+1)){
-                int i=1;
-                while(seen.count(val+i)) count++,i++;
+                while(seen.count(val+i)) i++;
             }
-            maxi=max(count,maxi);
+            maxi=max(i,maxi);
         }
         return maxi;
     }
